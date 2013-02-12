8f6c4c69d08816a3906f32c2a5a092d645b63dec
Vusile Terence Silonda <vusile@gmail.com>
2013-02-12 14:15:30 +0300

    Initial Commit

---
+++ application/controllers/Listings.php
@@ -0,0 +1,28 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Listings extends CI_Controller {
+
+	public function index()
+	{
+		
+	}
+
+	public function listingdetail($listing_id)
+	{
+
+	}
+
+	public function post_a_listing($type=0)
+	{
+
+	}
+
+	public function save_listing($type)
+	{
+
+	}
+
+}
+
+/* End of file controllername.php */
+/* Location: ./application/controllers/controllername.php */
