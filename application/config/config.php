4c3f6b2f49ab7f2b05531d78ad055c2dbea26ffe
vincent728 <daudivincent@yahoo.com>
2013-04-23 16:16:19 +0300

    resolving ip conflicts

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost:8888/zoomtanzania/';
+$config['base_url']	= 'http://192.168.1.128:8888/zoomtanzania/';
 
 /*
 |--------------------------------------------------------------------------
